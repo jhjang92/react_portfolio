5a4f25acd1c893a97e82de2ade67cd7228472cdc
jaehyuk Jang <jhjang92@kakao.com>
2020-08-29 03:16:05 +0900

    Updates

---
+++ precache-manifest.3003195ceec19c08da9e401c618bac13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d74dbd2814bf58b7ca6ddcec19be0e0",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "4e751f37e17962a51968",
+    "url": "/react_portfolio/static/css/main.9dbbb728.chunk.css"
+  },
+  {
+    "revision": "cc8337c4bc914afe298c",
+    "url": "/react_portfolio/static/js/2.c32ef620.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_portfolio/static/js/2.c32ef620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e751f37e17962a51968",
+    "url": "/react_portfolio/static/js/main.df53a086.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  }
+]);
